6f741905ddc23f36bae2b00425765c134599b4e1
Debaraj Rout <debrajrout28@gmail.com>
2023-08-12 19:02:11 +0530

    Its my first commit

---
+++ app/(dashboard)/[storeid]/(routes)/settings/page.tsx
@@ -0,0 +1,17 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface SettingPageProps {
+  params: {
+    storeId: string;
+  };
+}
+
+const SettingPage: React.FC<SettingPageProps> = ({ params }) => {
+  const { userId } = auth();
+  if (!userId) {
+    redirect("sign-in");
+  }
+  return <div>hi</div>;
+};
+export default SettingPage;
