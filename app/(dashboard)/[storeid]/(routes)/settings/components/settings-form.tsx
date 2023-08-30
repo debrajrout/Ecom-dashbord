7267b0ee5a933938a962a3214c4a94d31f9e5486
Debaraj Rout <debrajrout28@gmail.com>
2023-08-30 22:25:03 +0530

    Just a bit of a hack

--- app/(dashboard)/[storeid]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeid]/(routes)/settings/components/settings-form.tsx
@@ -1,3 +1,38 @@
-export const SettingsForm = () => {
-  return <div>Setting Form...</div>;
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Store } from "@prisma/client";
+import { Resolver } from "dns";
+import { Trash } from "lucide-react";
+
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+interface SettingsFormProps {
+  initialData: Store;
+}
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
+
+type SettingsFormValues = z.infer<typeof formSchema>;
+export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) =>  {
+  const form = useForm:React.FC<SettingsFormValues>({
+    Resolver:zodResolver
+  })
+  
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading title="Settings" description="manage store" />
+        <Button variant={"destructive"} size="sm" onClick={() => {}}>
+          <Trash className="h-4 w-4" />
+        </Button>
+      </div>
+      <Separator />
+    </>
+  );
 };
--- app/(dashboard)/[storeid]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeid]/(routes)/settings/page.tsx
@@ -28,7 +28,7 @@ const SettingPage: React.FC<SettingPageProps> = async ({ params }) => {
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <SettingsForm />
+        <SettingsForm initialData={store} />
       </div>
     </div>
   );
